681d00ee0d65b21f94f8915a0041e7fdb850380b
Julien Melissas <julien.melissas@gmail.com>
2017-01-15 23:35:10 -0500

    Added support for project videos with FitVids.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,7 +2,11 @@
  * Do stuff once the page has loaded!
  */
 $(document).ready(function() {
+  // Nav Button Toggle
   $('.nav-icon').click(function(event) {
     $(this).parent().toggleClass('open');
   });
+
+  // FitVids.js
+  $('.project-video').fitVids();
 });
