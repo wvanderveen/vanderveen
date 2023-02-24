187a92bd64b43e42315ca4fecbea96c03a1d9120
Julien Melissas <julien@arryved.com>
2023-02-24 00:41:55 -0600

    Copy images over (no compression)

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,4 +2,5 @@ let mix = require('laravel-mix');
 
 mix.js('assets/scripts/main.js', 'scripts')
    .sass('assets/styles/main.scss', 'styles').options({ processCssUrls: false })
+   .copy('assets/images', 'dist/images')
    .setPublicPath('dist');
